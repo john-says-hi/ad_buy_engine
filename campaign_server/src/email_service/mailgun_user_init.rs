5e475bf7ac1b1be8378aac32b982680e61a0eaa2
johnwaltonvi <your-email@example.com>
2026-06-27 12:59:38 -0700

    chore(security): remove committed secrets

--- campaign_server/src/email_service/mailgun_user_init.rs
+++ campaign_server/src/email_service/mailgun_user_init.rs
@@ -1,62 +1,24 @@
 use crate::utils::errors::ApiError;
-use ad_buy_engine::constant::apis::private::API_URL_CONFIRM_EMAIL_INVITATION_ADD_USER_TO_ACCOUNT;
 use ad_buy_engine::constant::apis::public::API_URL_CONFIRM_EMAIL_INVITATION;
 use ad_buy_engine::constant::email::email_service_info::{
-    EMAIL_SENDER_DOMAIN, EMAIL_SENDER_USERNAME, MAILGUN_API_KEY,
+    EMAIL_SENDER_DOMAIN, EMAIL_SENDER_DOMAIN_ENV, EMAIL_SENDER_USERNAME, EMAIL_SENDER_USERNAME_ENV,
+    MAILGUN_API_KEY_ENV,
 };
-use ad_buy_engine::constant::server_info::{CAMPAIGN_SERVER_URL, ROOT_DOMAIN};
+use ad_buy_engine::constant::server_info::ROOT_DOMAIN;
 use ad_buy_engine::data::backend_models::invitation::Invitation;
 use mailgun_rs::{EmailAddress, Mailgun, Message};
+use std::env;
 use std::error::Error;
 
-// pub fn send_email_invite_another_user(invitation: &TeamInvitation) -> Result<(), ApiError> {
-//     let recipient = invitation.email.as_str();
-//     let recipient = EmailAddress::address(&recipient);
-//     let message = Message {
-//         to: vec![recipient],
-//         subject: String::from("YOU ARE INVITED - RESPONSE REQUIRED"),
-//         html: format!(
-//             "Please click on the link below to complete your registration. <br/>
-//          <a href=\"{}/{}/{}/{}\">
-//          Verify Email</a>
-//          your Invitation expires on <strong>{}</strong>",
-//             CAMPAIGN_SERVER_URL,
-//             "email",
-//             "accept_invite",
-//             invitation.id,
-//             invitation
-//                 .expires_at
-//                 .format("%I:%M %p %A, %-d %B, %C%y")
-//                 .to_string(),
-//         ),
-//         ..Default::default()
-//     };
-//
-//     let client = Mailgun {
-//         api_key: String::from(MAILGUN_API_KEY),
-//         domain: String::from(EMAIL_SENDER_DOMAIN),
-//         message,
-//     };
-//     let email = EmailAddress::name_address(
-//         "no-reply",
-//         format!("{}@{}", EMAIL_SENDER_USERNAME, EMAIL_SENDER_DOMAIN).as_str(),
-//     );
-//     let sent_email = client.send(&email);
-//
-//     // Note that we only print out the error response from email api
-//     match sent_email {
-//         Ok(res) => {
-//             dbg!("Success: {}", res.message);
-//             Ok(())
-//         }
-//         Err(err) => {
-//             dbg!("Failed: {}", err.description());
-//             Err(ApiError::InternalServerError(err.description().to_string()))
-//         }
-//     }
-// }
-
 pub fn send_invitation(invitation: &Invitation) -> Result<(), ApiError> {
+    let mailgun_api_key = env::var(MAILGUN_API_KEY_ENV).map_err(|_| {
+        ApiError::InternalServerError(format!("{} must be set", MAILGUN_API_KEY_ENV))
+    })?;
+    let email_sender_domain =
+        env::var(EMAIL_SENDER_DOMAIN_ENV).unwrap_or_else(|_| EMAIL_SENDER_DOMAIN.to_string());
+    let email_sender_username =
+        env::var(EMAIL_SENDER_USERNAME_ENV).unwrap_or_else(|_| EMAIL_SENDER_USERNAME.to_string());
+
     let recipient = invitation.email.as_str();
     let recipient = EmailAddress::address(&recipient);
     let message = Message {
@@ -79,13 +41,13 @@ pub fn send_invitation(invitation: &Invitation) -> Result<(), ApiError> {
     };
 
     let client = Mailgun {
-        api_key: String::from(MAILGUN_API_KEY),
-        domain: String::from(EMAIL_SENDER_DOMAIN),
+        api_key: mailgun_api_key,
+        domain: email_sender_domain.clone(),
         message,
     };
     let email = EmailAddress::name_address(
-        EMAIL_SENDER_DOMAIN,
-        format!("{}@{}", EMAIL_SENDER_USERNAME, EMAIL_SENDER_DOMAIN).as_str(),
+        email_sender_domain.as_str(),
+        format!("{}@{}", email_sender_username, email_sender_domain).as_str(),
     );
 
     let sent_email = client.send(&email);
