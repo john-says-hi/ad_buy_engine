a4dbabeaa4b302b92d9901263977f947f0983355
johnwaltonvi <your-email@example.com>
2026-06-27 18:05:42 -0700

    chore: revert minimal click server prototype

--- runtime/click_server/src/http/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod auth_handlers;
-pub mod campaign_handlers;
-pub mod request_context;
-pub mod router;
-pub mod setup_handlers;
-pub mod stats_handlers;
-pub mod status_handlers;
-pub mod tracking_handlers;
